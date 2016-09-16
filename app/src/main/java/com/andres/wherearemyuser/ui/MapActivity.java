bd760efbdb64cbe125b9eedb6b4ce411f69393b2
Andres David <ads.f117@gmail.com>
2016-09-16 11:42:16 -0500

    show random user and button animate go to position

--- app/src/main/java/com/andres/wherearemyuser/ui/MapActivity.java
+++ app/src/main/java/com/andres/wherearemyuser/ui/MapActivity.java
@@ -1,5 +1,7 @@
 package com.andres.wherearemyuser.ui;
 
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -7,26 +9,54 @@ import android.view.View;
 import android.widget.Toast;
 
 import com.andres.wherearemyuser.R;
+import com.andres.wherearemyuser.Utils;
 import com.andres.wherearemyuser.dataobjects.User;
 import com.andres.wherearemyuser.webservices.GetUser;
 import com.andres.wherearemyuser.webservices.ServiceCreator;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 import java.util.List;
+import java.util.Random;
 
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
-public class MapActivity extends AppCompatActivity {
+public class MapActivity extends AppCompatActivity implements OnMapReadyCallback {
 
     private  final String LOG_TAG = MapActivity.class.toString();
+    GoogleMap m_map;
+    boolean mapReady=false;
+    private List<User> mUserList=null;
+    static final CameraPosition SEATTLE = CameraPosition.builder()
+            .target(new LatLng(47.6204,-122.2491))
+            .zoom(10)
+            .bearing(0)
+            .tilt(45)
+            .build();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
-
-        getUsersFromServer();
+        MapFragment mapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Generate Ramdom User", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+                ramdomUser();
+            }
+        });
     }
 
     public void getUsersFromServer() {
@@ -36,6 +66,8 @@ public class MapActivity extends AppCompatActivity {
             @Override
             public void onResponse(Call<List<User>> call, Response<List<User>> response) {
                 if (response.isSuccessful()) {
+                    mUserList = response.body();
+                    ramdomUser();
                     Log.d(LOG_TAG,"isSuccessful Response"+response.body().get(0).getName());
                 } else {
                     Log.e(LOG_TAG,"onUsersFail");
@@ -50,5 +82,34 @@ public class MapActivity extends AppCompatActivity {
     }
     public void onNetworkError() {
         Toast.makeText(this,"No internet connection,Please Check you Internet Conecction",Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mapReady=true;
+        m_map = googleMap;
+        getUsersFromServer();
+    }
+
+    private void ramdomUser()
+    {
+        User ramdomUser = mUserList.get(new Utils().generateRamdomNumber(mUserList.size()));
+        createMarker (ramdomUser.getAddress().getGeo().getLng(),ramdomUser.getAddress().getGeo().getLat(),ramdomUser.getName());
+
+    }
+    protected void  createMarker(double latitude, double longitude, String title) {
+
+        m_map.clear();
+        LatLng latLng = new LatLng(latitude, longitude);
+         m_map.addMarker(new MarkerOptions()
+                .position(latLng)
+                .title(title));
+        CameraPosition cameraPosition = CameraPosition.builder()
+                .target(latLng)
+                .bearing(0)
+                .tilt(45)
+                .build();
+        m_map.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition), 3000, null);
+
     }
 }
